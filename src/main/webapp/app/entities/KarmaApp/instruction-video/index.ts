a89913c0deaccec7ed4fc052bc5b1669b524a835
sarangibalu <sarangibalu.a@lxisoft.com>
2019-08-30 15:48:41 +0530

    KAR-38,KAR-75,KAR-87

--- src/main/webapp/app/entities/KarmaApp/instruction-video/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './instruction-video.service';
-export * from './instruction-video-update.component';
-export * from './instruction-video-delete-dialog.component';
-export * from './instruction-video-detail.component';
-export * from './instruction-video.component';
-export * from './instruction-video.route';
